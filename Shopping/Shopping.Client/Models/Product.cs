9942b839bcb1fc4728afc4eb182c4c9719234e7e
zeeshannaseem <zeshannaseem@gmail.com>
2025-05-24 20:26:30 -0500

    Initial commit

---
+++ Shopping/Shopping.Client/Models/Product.cs
@@ -0,0 +1,12 @@
+﻿namespace Shopping.Client.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+        public decimal Price { get; set; }
+    }
+}
